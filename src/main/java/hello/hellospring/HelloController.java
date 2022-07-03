f52e88fe17b2ef3912a7e9ce5ec651a284c4ac36
elogio.mi <elogio.mi@gmail.com>
2022-07-03 21:39:09 +0900

    Initial commit

---
+++ src/main/java/hello/hellospring/HelloController.java
@@ -0,0 +1,44 @@
+package hello.hellospring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hellospring(Model model){
+        model.addAttribute("data", "spring");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String hellomvc(@RequestParam("name") String name, Model model){
+        return "hellomvc";
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloapi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+
+        return hello;
+    }
+
+    static class Hello{//property 접근 방식, 빈 방식.
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+}
